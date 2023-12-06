da85c76367f00e608b2634d5ac4a7201dd3544ec
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-06 19:27:25 +0530

    set  up dark mode and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import AuthPage from './pages/AuthPage/AuthPage';
+
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/auth' element={<AuthPage />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
