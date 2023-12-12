5cddd907f5b931eece29bd090975dfec46c5f39b
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-12 14:47:22 +0530

    made the feed post page, added like function , made props for diff users

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -3,10 +3,10 @@ import FeedPost from '../../components/FeedPosts/FeedPost';
 const FeedPosts = () => {
   return (
     <Container maxW={'container.sm'} py={10} px={2}>
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      <FeedPost img='/img1.png' username='janeaMaria_' avatar='/img1.png' />
+      <FeedPost img='/img2.png' username='_iamGod31' avatar='/img2.png' />
+      <FeedPost img='/img3.png' username='SoPhia_2' avatar='/img3.png' />
+      <FeedPost img='/img4.png' username='nature_lover_71' avatar='/img4.png' />
     </Container>
   );
 };
