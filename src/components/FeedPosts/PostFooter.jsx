fdba6be4ae2982e163c104591479f86a6b221967
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-16 14:55:27 +0530

    made the profile pic modal

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -14,7 +14,7 @@ import {
   UnlikeLogo,
 } from '../../assets/constants';
 
-const PostFooter = ({ username }) => {
+const PostFooter = ({ username, isProfilePage }) => {
   const [liked, setLiked] = useState(false);
   const [likes, setLikes] = useState(1000);
 
@@ -29,7 +29,7 @@ const PostFooter = ({ username }) => {
   };
 
   return (
-    <Box mb={10}>
+    <Box mb={10} mt={'auto'}>
       <Flex alignItems={'center'} gap={4} w={'full'} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={'pointer'} fontSize={18}>
           {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -42,15 +42,19 @@ const PostFooter = ({ username }) => {
         {likes} likes
       </Text>
 
-      <Text fontSize={'sm'} fontWeight={700}>
-        {username}{' '}
-        <Text as='span' fontWeight={400}>
-          Feeling Good
-        </Text>
-        <Text fontSize={'sm'} color={'gray'}>
-          View all 1,000 comments
-        </Text>
-      </Text>
+      {!isProfilePage && (
+        <>
+          <Text fontSize={'sm'} fontWeight={700}>
+            {username}{' '}
+            <Text as='span' fontWeight={400}>
+              Feeling Good
+            </Text>
+            <Text fontSize={'sm'} color={'gray'}>
+              View all 1,000 comments
+            </Text>
+          </Text>
+        </>
+      )}
 
       <Flex
         alignItems={'center'}
--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,48 +1,138 @@
-import { GridItem, Flex, Text, Image } from '@chakra-ui/react';
+import {
+  GridItem,
+  Flex,
+  Text,
+  Image,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalCloseButton,
+  ModalBody,
+  Box,
+  Avatar,
+  Divider,
+  VStack,
+} from '@chakra-ui/react';
 import { AiFillHeart } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 import { FaComment } from 'react-icons/fa';
+import Comment from '../Comment/Comment';
+import PostFooter from '../FeedPosts/PostFooter';
 const ProfilePost = ({ img }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <GridItem
-      borderRadius={4}
-      cursor={'pointer'}
-      overflow={'hidden'}
-      border={'1px solid'}
-      borderColor={'whiteAlpha.300'}
-      position={'relative'}
-      aspectRatio={1 / 1}>
-      <Flex
-        opacity={0}
-        _hover={{ opacity: 1 }}
-        position={'absolute'}
-        top={0}
-        left={0}
-        right={0}
-        bottom={0}
-        bg={'blackAlpha.700'}
-        transition={'all 0.3s ease'}
-        zIndex={1}
-        justifyContent={'center'}>
-        <Flex alignItems={'center'} justifyContent={'center'} gap={50}>
-          <Flex>
-            <AiFillHeart size={25} />
-            <Text fontWeight={'bold'}> 7</Text>
-          </Flex>
-          <Flex>
-            <FaComment size={25} />
-            <Text fontWeight={'bold'}> 7</Text>
+    <>
+      <GridItem
+        borderRadius={4}
+        cursor={'pointer'}
+        overflow={'hidden'}
+        border={'1px solid'}
+        borderColor={'whiteAlpha.300'}
+        position={'relative'}
+        aspectRatio={1 / 1}
+        onClick={onOpen}>
+        <Flex
+          opacity={0}
+          _hover={{ opacity: 1 }}
+          position={'absolute'}
+          top={0}
+          left={0}
+          right={0}
+          bottom={0}
+          bg={'blackAlpha.700'}
+          transition={'all 0.3s ease'}
+          zIndex={1}
+          justifyContent={'center'}>
+          <Flex alignItems={'center'} justifyContent={'center'} gap={50}>
+            <Flex>
+              <AiFillHeart size={25} />
+              <Text fontWeight={'bold'}> 7</Text>
+            </Flex>
+            <Flex>
+              <FaComment size={25} />
+              <Text fontWeight={'bold'}> 7</Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
 
-      <Image
-        src={img}
-        alt='profile-pic'
-        w={'100%'}
-        h={'100%'}
-        objectFit={'cover'}
-      />
-    </GridItem>
+        <Image
+          src={img}
+          alt='profile-pic'
+          w={'100%'}
+          h={'100%'}
+          objectFit={'cover'}
+        />
+      </GridItem>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        size={{ base: '3xl', md: '5xl' }}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody bg={'black'} pb={'5'}>
+            <Flex
+              gap={'4'}
+              w={{ base: '90%', small: '70%', md: 'full' }}
+              mx={'auto'}>
+              <Box
+                borderRadius={4}
+                overflow={'hidden'}
+                border={'1px solid'}
+                borderColor={'whiteAlpha.300'}
+                flex={1.5}>
+                <Image src={img} alt='profile post' />
+              </Box>
+              <Flex
+                flex={1}
+                flexDir={'column'}
+                px={10}
+                display={{ base: 'none', md: 'flex' }}>
+                <Flex alignItems={'center'} justifyContent={'space-between'}>
+                  <Flex alignItems={'center'} gap={4}>
+                    <Avatar
+                      src='/profilepic.png'
+                      size={'sm'}
+                      name='name of pic'
+                    />
+                    <Text fontWeight={'bold'} fontSize={12}>
+                      username1234
+                    </Text>
+                  </Flex>
+
+                  <Box
+                    _hover={{ bg: 'whiteAlpha.300', color: 'red.600' }}
+                    borderRadius={4}
+                    p={1}>
+                    <MdDelete size={20} cursor={'pointer'} />
+                  </Box>
+                </Flex>
+                <Divider my={4} bg={'gray.500'} />
+
+                <VStack
+                  w={'full'}
+                  alignItems={'start'}
+                  maxH={'350px'}
+                  overflowY={'auto'}>
+                  <Comment
+                    createdAt='1d ago'
+                    username='_iamsrk'
+                    profliePic='/profilepic.png'
+                    text='dummy pic from upslacsh'
+                  />
+                </VStack>
+                <Divider my={4} bg={'gray.800'} />
+
+                <PostFooter isProfilePage={true} />
+              </Flex>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 export default ProfilePost;
