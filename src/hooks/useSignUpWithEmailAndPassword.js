081dd88745b1747dff734780311bc5c520c05332
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-17 17:37:09 +0530

    implemented unique username logic using query feature of firebase

--- src/hooks/useSignUpWithEmailAndPassword.js
+++ src/hooks/useSignUpWithEmailAndPassword.js
@@ -1,6 +1,6 @@
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth, firestore } from '../components/firebase/firebase';
-import { doc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, query, setDoc, where } from 'firebase/firestore';
 import useShowToast from './useShowToast';
 import useAuthStore from '../store/AuthStore';
 
@@ -9,13 +9,22 @@ const useSignUpWithEmailAndPassword = () => {
     const showToast = useShowToast();
     const loginUser = useAuthStore(state => state.login)// using login from authstore using zustand
 
-    
+
     const signup = async (inputs) => {
         if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
             showToast("Error", "Please fill all the feilds", "error")
             return
         }
 
+        const userRef = collection(firestore, "users")//creates a reference to the users arryobj in the db
+        const q = query(userRef, where('username', '==', inputs.username));
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty == false) {
+            showToast("Error", "Username already exists", "error");
+            return
+        }
+        
         try {
             const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
             if (!newUser && error) {
