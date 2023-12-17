093108dfd5ea5fec29397e56db08d0a592f8e233
Tushar Gupta <tushargupta4870@gmail.com>
2023-12-17 20:55:11 +0530

    added the useLogin hook and implemented login feature

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -1,11 +1,13 @@
-import { Button, Input } from '@chakra-ui/react';
+import { Alert, AlertIcon, Button, Input } from '@chakra-ui/react';
 import { useState } from 'react';
+import useLogin from '../../hooks/useLogin';
 
 const Login = () => {
   const [inputs, setInputs] = useState({
     email: '',
     password: '',
   });
+  const { loading, error, login } = useLogin();
   return (
     <>
       <Input
@@ -25,7 +27,20 @@ const Login = () => {
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
 
-      <Button width={'full'} colorScheme='blue' size={'sm'} fontSize={15}>
+      {error && (
+        <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+
+      <Button
+        width={'full'}
+        colorScheme='blue'
+        size={'sm'}
+        fontSize={15}
+        isLoading={loading}
+        onClick={() => login(inputs)}>
         Login
       </Button>
     </>
